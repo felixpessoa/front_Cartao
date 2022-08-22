8671a3292923dd429c7d9459c2cba24bde5cac63
felixpessoa <felixpessoa90@gmail.com>
2022-08-22 14:51:22 -0300

    realizado ajuste no navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './components/template/header/header.component';
@@ -19,7 +22,8 @@ import { NavComponent } from './components/template/nav/nav.component';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -28,7 +32,11 @@ import { NavComponent } from './components/template/nav/nav.component';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatDividerModule,
+    MatButtonModule,
+    LayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/template/nav/nav.component.ts
+++ src/app/components/template/nav/nav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor() { }
+  constructor(
+    private observer: BreakpointObserver
+  ) { }
 
   ngOnInit(): void {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
   }
 
+
 }
+
